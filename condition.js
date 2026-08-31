b20ba13c0fa72649f5c26778bf4388dd750b32e3
Mrinmoydatta <mrinmoydatta2005@gmail.com>
2026-08-31 22:57:58 +0530

    if else

--- condition.js
+++ condition.js
@@ -4,3 +4,10 @@ let age=19;
 if(age>18){
     console.log("Your are eligible to vote.");
 }
+
+// if-else statement
+if(age>18){
+    console.log("eligible to vote");
+}else{
+    console.log("not eligible to vote");
+}
